aee6837283f2c407f898a269b45853c30aa11875
Cheikhna-sakho <cheikh.bsakho@gmail.com>
2022-11-10 01:13:25 +0100

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react'
-import { RiArrowGoBackLine,RiArrowGoForwardLine } from "react-icons/ri";
+import React, { useEffect, useRef, useState } from 'react'
+import { RiArrowGoBackLine, RiArrowGoForwardLine } from "react-icons/ri";
+import { pngTask } from '../api/task.api';
 import { TaskDataContext } from '../contexts/TaskContext';
+// import { activeClass } from '../utils/activeClass';
 const Pagination = ({ limit }) => {
-    const {count} = TaskDataContext();
-    const [page,setPage] =useState(0);
+    const { count, setTask } = TaskDataContext();
+    const [page, setPage] = useState(0);
     const maxPage = Math.ceil(count / limit);
     const skip = limit * page;
-    console.log("max",count);
+    const pageItem = useRef([]);
+
+    useEffect(() => {
+        const currentEl = pageItem.current
+        currentEl.filter(el => {
+            if (el) {
+                if (currentEl.indexOf(el) !== page) {
+                    if (el.className.includes("active")) {
+                        console.log("yes is active", el.className.split(" ").filter(cl => cl != "active"));
+                        el.className = el.className.split(" ").filter(cl => cl != "active").join(" ");
+                    }
+                }
+                else {
+                    el.className += " active"
+                }
+            }
+            return el;
+        });
+        (async () => {
+            try {
+                const res = await pngTask(limit, skip);
+                setTask(res.data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, [page]);
+
     return (
         <div className='pagination flex'>
-            <p className='page-item'><RiArrowGoBackLine/></p>
-            {[...Array(maxPage).keys()].map(i => <p className='page-item' key={i} onClick={()=> setPage(i+1)}>{i+1}</p>)}
-            <p className='page-item'><RiArrowGoForwardLine/></p>
-        </div>
+            <p onClick={() => page > 0 && setPage(page - 1)} className='page-item'><RiArrowGoBackLine /></p>
+            {[...Array(maxPage).keys()].map(i => <p ref={el => pageItem.current[i] = el} className='page-item' key={i} onClick={() => setPage(i)}>{i + 1}</p>)}
+            <p onClick={() => page < maxPage && setPage(page + 1)} className='page-item'><RiArrowGoForwardLine /></p>
+            <p>Page {page + 1}</p>
+        </div >
     )
 }
 
