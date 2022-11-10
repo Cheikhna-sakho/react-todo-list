c1415f5a7cceef0120527c39215782eb0ec98056
Cheikhna-sakho <cheikh.bsakho@gmail.com>
2022-11-10 01:15:24 +0100

    task functionalityn

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 import { BsPencilSquare } from "react-icons/bs";
-import { TiDelete } from "react-icons/ti";
+// import { TiDelete } from "react-icons/ti";
 import { FcCancel } from "react-icons/fc";
 import { IoTrashBin } from "react-icons/io5";
 import { removeTask, updateTask } from '../api/task.api';
@@ -9,11 +9,13 @@ import { activeClass, disableClass } from '../utils/activeClass';
 import { TaskDataContext } from '../contexts/TaskContext';
 const TodoItem = ({ item }) => {
   const { description = "", completed = "", _id = "" } = item;
+  const [checked,setChecked] = useState(completed);
   const { task, setTask } = TaskDataContext();
   const taskIndex = task.indexOf(item);
   const [upTask, setUpTask] = useState(description);
   const modify = useRef(null);
   const completedTask = () => {
+    setChecked(!checked);
     if (!completed) {
       onUpdateTask({ completed: true })
       return;
@@ -25,15 +27,18 @@ const TodoItem = ({ item }) => {
     try {
       delete task[taskIndex];
       setTask(taskData => [...taskData.filter(todo => todo && todo)]);
-      const res = await removeTask(id);
+      await removeTask(id);
     } catch (error) {
       console.log(error);
     }
   }
   const onUpdateTask = async (data) => {
     try {
-      const res = await updateTask(_id, data);
-      
+      await updateTask(_id, data);
+      if (data?.description) {
+        task[taskIndex].description = data.description;
+        setTask(taskData => [...taskData.filter(todo => todo && todo)]);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -45,7 +50,7 @@ const TodoItem = ({ item }) => {
       <div className="desc-task flex">
         <p className='flex'>
           <span onClick={completedTask}>
-            {completed ? <ImCheckboxChecked color='green' /> : <ImCheckboxUnchecked />}
+            {checked ? <ImCheckboxChecked color='green' /> : <ImCheckboxUnchecked />}
           </span>
           {description}
         </p>
